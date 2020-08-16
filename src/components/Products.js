bd75227e974eb31a4c35f4a4212b61d51f34b8d6
Panotsingsith <Toe@gmail.com>
2020-08-16 12:36:44 +0700

    first commit

---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Products extends Component {
+    render() {
+        return (
+            <div>
+                <h2>สินค้า</h2>
+            </div>
+        )
+    }
+}
